2f9e44b01fcac36e8c4664da2e080afe590328f5
tomas <totobarajas124@gmail.com>
2026-06-08 20:11:43 -0300

    feat: create dynamic car detail page with financing call-to-action and specification display

--- app/cars/[id]/page.jsx
+++ app/cars/[id]/page.jsx
@@ -11,7 +11,7 @@ export default function CardIdPage({ params }) {
 
   const { setCarFromForm } = useCarForm()
 
-  const {getCarbyId} = useContext(CarsContext);
+  const { getCarbyId } = useContext(CarsContext);
 
   const [currentCar, setCurrentCar] = useState(null);
   const id = params.id;
@@ -22,7 +22,7 @@ export default function CardIdPage({ params }) {
       const response = await getCarbyId(id); // <-- tu función que trae el producto
       setCurrentCar(response);
     }
-  
+
     loadCar();
   }, [])
 
@@ -32,53 +32,54 @@ export default function CardIdPage({ params }) {
 
   return (
     <main className="pt-18 flex flex-col">
-        {currentCar ? 
+      {currentCar ?
         <main className="pt-18 flex flex-col">
-        <section className="flex flex-col md:flex-row items-center justify-around">
-          <CarruselImg product={currentCar} />
+          <section className="flex flex-col md:flex-row items-center justify-around">
+            <CarruselImg product={currentCar} />
             <div className="flex flex-col shadow-black/50 shadow-xl md:py-12 md:px-16 md:m-0 m-6 px-8 py-4">
               <h1 className="text-3xl font-semibold">{currentCar.marca} {currentCar.modelo}</h1>
               <div className="flex flex-col gap-2 my-5">
-                <small className="text-sm">Precio contado</small>
+                <p>Precio: Consultar</p>
+                {/* <small className="text-sm">Precio contado</small>
                 <b className="text-2xl"> ${currentCar.precio.toLocaleString("es-AR")}</b>
                 <small className="text-sm">Anticipo</small>
-                <small> ${currentCar.anticipo.toLocaleString("es-AR")}</small>
+                <small> ${currentCar.anticipo.toLocaleString("es-AR")}</small> */}
                 <div className="flex flex-col text-lg gap-2 my-5">
-                <p>Año: {currentCar.anio}</p>
-                <p>Kilometraje: {currentCar.km.toLocaleString("es-AR")}</p>
+                  <p>Año: {currentCar.anio}</p>
+                  <p>Kilometraje: {currentCar.km.toLocaleString("es-AR")}</p>
                 </div>
               </div>
-            <Link onClick={handleClick} className="bg-red-500 text-white font-bold px-10 text-center py-2 cursor-pointer rounded-xl mx-auto w-full" href={`/financing`}>Consultar financiamiento</Link>
+              <Link onClick={handleClick} className="bg-red-500 text-white font-bold px-10 text-center py-2 cursor-pointer rounded-xl mx-auto w-full" href={`/financing`}>Consultar financiamiento</Link>
             </div>
-        </section>
-        <section className="md:w-full px-10 mt-10">
+          </section>
+          <section className="md:w-full px-10 mt-10">
             <div>
-            <h3 className="font-bold text-2xl mb-7">Especificaciones</h3>
+              <h3 className="font-bold text-2xl mb-7">Especificaciones</h3>
             </div>
             <div className="flex justify-between flex-col md:flex-row md:items-center items-start text-lg gap-10">
-                <ul>
-                    <li>Motor: {currentCar.motor}</li>
-                    <li>Transmisión: {currentCar.transmision}</li>
-                    <li>Puertas: {currentCar.puertas}</li>
-                    <li>Combustible: {currentCar.combustible}</li>
-                </ul>
-                <ul>
-                    <li>Carroceria: {currentCar.carroceria}</li>
-                    <li>Direccion hidraulica: {currentCar.hidraulica ===  1 ? "Si" : "No"}</li>
-                    <li>Sensor de estacionamiento: {currentCar.sensor === 1 ? "Si" : "No"}</li>
-                    <li>Aire Acondicionado: {currentCar.aire_acondicionado === 1 ? "Si" : "No"}</li>
-                </ul>
-                <ul>
-                    <li>Tipo Frenos ABS: {currentCar.frenos === 1 ? "Si" : "No"}</li>
-                    <li>Bolsas de Aire Frontales: {currentCar.airbag === 1 ?"Si" : "No"}</li>
-                    <li>Acepta permuta: {currentCar.permuta === 1 ?"Si" : "No"}</li>
-                    <li>Estereo con pantalla: {currentCar.estereo_pantalla === 1 ?"Si" : "No"}</li>
-                </ul>
+              <ul>
+                <li>Motor: {currentCar.motor}</li>
+                <li>Transmisión: {currentCar.transmision}</li>
+                <li>Puertas: {currentCar.puertas}</li>
+                <li>Combustible: {currentCar.combustible}</li>
+              </ul>
+              <ul>
+                <li>Carroceria: {currentCar.carroceria}</li>
+                <li>Direccion hidraulica: {currentCar.hidraulica === 1 ? "Si" : "No"}</li>
+                <li>Sensor de estacionamiento: {currentCar.sensor === 1 ? "Si" : "No"}</li>
+                <li>Aire Acondicionado: {currentCar.aire_acondicionado === 1 ? "Si" : "No"}</li>
+              </ul>
+              <ul>
+                <li>Tipo Frenos ABS: {currentCar.frenos === 1 ? "Si" : "No"}</li>
+                <li>Bolsas de Aire Frontales: {currentCar.airbag === 1 ? "Si" : "No"}</li>
+                <li>Acepta permuta: {currentCar.permuta === 1 ? "Si" : "No"}</li>
+                <li>Estereo con pantalla: {currentCar.estereo_pantalla === 1 ? "Si" : "No"}</li>
+              </ul>
             </div>
-        </section>
-    </main> :
-    <Loading />
-    }
+          </section>
+        </main> :
+        <Loading />
+      }
     </main>
   )
 }
